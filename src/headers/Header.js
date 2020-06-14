0438d5dac059c7d13947b23e830f2e038d05adfb
sakshamgoyal06 <sakshamgoyal06@gmail.com>
2020-06-14 23:42:55 +0530

    Initialize project using Create React App

---
+++ src/headers/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header className="App-header">
+        <p>
+          Edit and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
